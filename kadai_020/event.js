aa1d36aaea9cf2074fcac67dcf6ed6fb944eedf6
Misaokilife <146902325+Misaokilife@users.noreply.github.com>
2023-12-04 22:37:12 +0900

    kadai_020を削除
    誤ファイルをアップロードしてしまった為、削除

--- kadai_020/event.js
+++
@@ -1,7 +0,0 @@
-const Btn = document.getElementById('btn');
-const showText = document.getElementById('text');
-
-Btn.addEventListener('click', () => {
-    showText.textContent = 'ボタンをクリックしました';
-});
-
